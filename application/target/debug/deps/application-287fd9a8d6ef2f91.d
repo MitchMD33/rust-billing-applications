e27197bdc54ff514ef04e624f4c5ccd30b1b9015
MitchMD33 <brown.l.mitchell@outlook.com>
2025-04-19 15:14:17 +0800

    crated the main file for the application

---
+++ application/target/debug/deps/application-287fd9a8d6ef2f91.d
@@ -0,0 +1,5 @@
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\libapplication-287fd9a8d6ef2f91.rmeta: src\main.rs
+
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\application-287fd9a8d6ef2f91.d: src\main.rs
+
+src\main.rs:
---
+++ application/target/debug/deps/application-6c9d3714bc1e5557.d
@@ -0,0 +1,5 @@
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\libapplication-6c9d3714bc1e5557.rmeta: src\main.rs
+
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\application-6c9d3714bc1e5557.d: src\main.rs
+
+src\main.rs:
