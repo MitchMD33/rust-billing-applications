cbd3be901a6efedcda5a82aecbb2055bfa1b3773
MitchMD33 <brown.l.mitchell@outlook.com>
2025-04-26 14:23:14 +0800

    Completed the mini project

---
+++ application/target/debug/application.d
@@ -0,0 +1,1 @@
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\application.exe: L:\Rust-Billing-Applications\rust-billing-applications\application\src\main.rs
---
+++ application/target/debug/deps/application-91958c4ee634ab43.d
@@ -0,0 +1,5 @@
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\application-91958c4ee634ab43.exe: src\main.rs
+
+L:\Rust-Billing-Applications\rust-billing-applications\application\target\debug\deps\application-91958c4ee634ab43.d: src\main.rs
+
+src\main.rs:
